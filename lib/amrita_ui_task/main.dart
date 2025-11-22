3416d4cee158f0c1db150fefb4e090cc898be623
Kaish02 <193617774+Kaish02@users.noreply.github.com>
2025-11-22 11:54:46 +0530

    ui task complets

---
+++ lib/amrita_ui_task/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'screen/edit_profile.dart';
+import 'screen/theme/app_theme.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: EditProfile(),
+      themeMode: ThemeMode.system,
+      theme: AppTheme.darkTheme,
+      darkTheme: AppTheme.lightTheme,
+    );
+  }
+}
