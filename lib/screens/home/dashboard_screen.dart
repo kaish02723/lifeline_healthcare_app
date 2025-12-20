8a151a6c772b0f2480be79d3ba071ee4136f756c
jahir123695 <mdj012694@gmail.com>
2025-12-20 22:33:49 +0530

    changes in dashboard screen

--- lib/screens/home/dashboard_screen.dart
+++ lib/screens/home/dashboard_screen.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lifeline_healthcare_app/config/color.dart';
 import 'package:lifeline_healthcare_app/providers/dashboard_provider.dart';
 import 'package:lifeline_healthcare_app/screens/appointments/appointment_surgery_booking_screen.dart';
 import 'package:lifeline_healthcare_app/screens/appointments/my_appointment_screen.dart';
@@ -12,7 +14,6 @@ import 'package:lifeline_healthcare_app/screens/home/user_profile_screen.dart';
 import 'package:lifeline_healthcare_app/screens/home/medicine%20screen/medicine_order_detail_screen.dart';
 import 'package:lifeline_healthcare_app/screens/patient/patient_consult_screen.dart';
 import 'package:lifeline_healthcare_app/screens/patient/patient_lab_test_screen.dart';
-import 'package:lifeline_healthcare_app/screens/patient/patient_medicine_category_screen.dart';
 import 'package:lifeline_healthcare_app/screens/patient/patient_my_labtest_screen.dart';
 import 'package:lifeline_healthcare_app/screens/patient/patient_my_surgery_screen.dart';
 import 'package:lifeline_healthcare_app/screens/patient/patient_physical_screen.dart';
@@ -20,7 +21,6 @@ import 'package:lifeline_healthcare_app/widgets/dashboard_widgets/dashboard_serv
 import 'package:lifeline_healthcare_app/widgets/dashboard_widgets/offer_banner.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
 import '../../providers/rating_provider/app_rating_review_provider.dart';
 import '../../providers/user_detail/get_userdetail_provider.dart';
 import '../../widgets/dashboard_widgets/dashboard_find_doctor_card.dart';
@@ -51,8 +51,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
       listen: false,
     ).getUserDetail(context);
 
-    Provider.of<TopRatingProvider>(context, listen: false)
-        .getTopRatings();
+    Provider.of<TopRatingProvider>(context, listen: false).getTopRatings();
     startOfferAutoScroll(context);
   }
 
@@ -64,6 +63,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
     var provider = Provider.of<GetUserDetailProvider>(context);
     var userData = provider.user;
     var dashBoardProvider = Provider.of<DashBoardProvider>(context);
@@ -92,11 +92,30 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   },
                   child: Row(
                     children: [
-                      const CircleAvatar(
+                      CircleAvatar(
                         radius: 32,
-                        backgroundImage: NetworkImage(
-                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuNhTZJTtkR6b-ADMhmzPvVwaLuLdz273wvQ&s',
-                        ),
+                        backgroundColor: AppColors.primary.withOpacity(0.15),
+                        backgroundImage:
+                            (userData?.picture != null &&
+                                    userData!.picture!.isNotEmpty)
+                                ? NetworkImage(
+                                  userData.picture!.startsWith("http")
+                                      ? userData.picture!
+                                      : "https://phone-auth-with-jwt-4.onrender.com${userData.picture!}",
+                                )
+                                : null,
+                        child:
+                            (userData?.picture == null ||
+                                    userData!.picture!.isEmpty)
+                                ? Icon(
+                                  Icons.person,
+                                  size: 32,
+                                  color:
+                                      isDark
+                                          ? AppColors.iconDark
+                                          : AppColors.icon,
+                                )
+                                : null,
                       ),
                       const SizedBox(width: 12),
                       Expanded(
@@ -244,11 +263,23 @@ class _DashboardScreenState extends State<DashboardScreen> {
           padding: const EdgeInsets.all(8.0),
           child: GestureDetector(
             onTap: () => _scaffoldKey.currentState!.openDrawer(),
-            child: const CircleAvatar(
+            child: CircleAvatar(
               backgroundColor: Colors.white,
-              backgroundImage: NetworkImage(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuNhTZJTtkR6b-ADMhmzPvVwaLuLdz273wvQ&s',
-              ),
+              backgroundImage:
+                  (userData?.picture != null && userData!.picture!.isNotEmpty)
+                      ? CachedNetworkImageProvider(
+                        userData.picture!.startsWith("http")
+                            ? userData.picture!
+                            : "https://phone-auth-with-jwt-4.onrender.com${userData.picture!}",
+                      )
+                      : null,
+              child:
+                  (userData?.picture == null || userData!.picture!.isEmpty)
+                      ? Icon(
+                        Icons.person,
+                        color: isDark ? AppColors.iconDark : AppColors.icon,
+                      )
+                      : null,
             ),
           ),
         ),
